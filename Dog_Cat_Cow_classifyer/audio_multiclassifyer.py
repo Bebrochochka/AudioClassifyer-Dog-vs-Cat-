4c54c3b4206307933804868a5101bdfd38e2c32e
Bebrochochka <119063145+Bebrochochka@users.noreply.github.com>
2025-09-28 21:17:16 -0500

    audio_multiclassifyer.py

--- Dog_Cat_Cow_classifyer/audio_multiclassifyer.py
+++ Dog_Cat_Cow_classifyer/audio_multiclassifyer.py
@@ -19,7 +19,7 @@ import matplotlib.pyplot as plt
 
 max_len = 88200
 
-model = load_model("multiclass_audio.h5")
+model = load_model("ML_projects009/multiclass_audio.h5")
 
 def prepare_data(smaple):
   sound, sr = librosa.load(smaple)
